3100abb293286aa98180ed4823320fe349477ef1
김동진 <ladmusician.kim@gmail.com>
2015-07-08 17:43:12 +0900

    bind mysql test 1

--- routes/index.js
+++ routes/index.js
@@ -1,32 +1,22 @@
 var express = require('express');
 var router = express.Router();
-
-
 var mysql = require('mysql');
-var connection = mysql.createConnection({
+var conn = mysql.createConnection({
     host :'ap-cdbr-azure-east-c.cloudapp.net',
     port : 3306,
     user : 'beb66692cdc551',
     password : '215db494',
     database:'newgeneration'
 });
-connection.connect(function(err) {
-    if (err) {
-        console.error('mysql connection error');
-        console.error(err);
-        throw err;
-    }
-});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  console.log('a');
-  var query = connection.query('select * from users',function(err,rows){
-        console.log(rows);
-        res.json(rows);
-    });
-    console.log(query);
-  res.render('index', { title: 'Express' });
+  conn.query('select * from Test', function (err, rows, fields) {
+  if (err) throw err; 
+  console.log(rows);
+  res.render('index', { title: 'select * from  -> rows.length : ' + rows.length });
+ });
+  //res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
